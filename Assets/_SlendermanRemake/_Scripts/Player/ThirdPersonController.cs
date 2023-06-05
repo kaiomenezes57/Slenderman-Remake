039ce0709a72ad7cb14b020e5278af22e0acbfde
Kaio Menezes <73968667+kaiomenezes57@users.noreply.github.com>
2023-06-05 03:15:46 -0300

    feat: update third person controller script folder part2

---
+++ Assets/_SlendermanRemake/_Scripts/Player/ThirdPersonController.cs
@@ -0,0 +1,101 @@
+﻿using UnityEditor;
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM 
+using UnityEngine.InputSystem;
+#endif
+
+namespace StarterAssets
+{
+    [RequireComponent(typeof(CharacterController))]
+#if ENABLE_INPUT_SYSTEM 
+    [RequireComponent(typeof(PlayerInput))]
+#endif
+    public class ThirdPersonController : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 2f;
+        [SerializeField] private float sprintSpeed = 4f;
+        [Range(0.0f, 0.3f)] [SerializeField] private float rotationSmoothTime = 0.12f;
+        [SerializeField] private float speedChangeRate = 10.0f;
+
+        private readonly float gravity = 9.81f;
+
+        [SerializeField] private LayerMask groundLayers;
+
+        private float _speed;
+        private float _animationBlend;
+        private float _targetRotation = 0.0f;
+        private float _rotationVelocity;
+        private float _verticalVelocity;
+
+        private int _animIDSpeed;
+        private int _animIDMotionSpeed;
+
+        private Animator _animator;
+        private CharacterController _controller;
+        private StarterAssetsInputs _input;
+
+        private Vector3 verticalVelocity;
+
+        private void Start()
+        {
+            _animator = GetComponentInChildren<Animator>();
+            _controller = GetComponent<CharacterController>();
+            _input = GetComponent<StarterAssetsInputs>();
+
+            _animIDSpeed = Animator.StringToHash("Speed");
+        }
+
+        private void Update()
+        {
+            GravityHandler();
+            MoveHandler();
+        }
+
+        private void MoveHandler()
+        {
+            float targetSpeed = _input.sprint ? sprintSpeed : moveSpeed;
+            if (_input.move == Vector2.zero) targetSpeed = 0.0f;
+
+            float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
+
+            float speedOffset = 0.1f;
+            float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
+
+            if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
+            {
+                _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * speedChangeRate);
+                _speed = Mathf.Round(_speed * 1000f) / 1000f;
+            }
+            else
+            {
+                _speed = targetSpeed;
+            }
+
+            _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * speedChangeRate);
+            if (_animationBlend < 0.01f) _animationBlend = 0f;
+
+            Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
+            
+            if (_input.move != Vector2.zero)
+            {
+                _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+                float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, rotationSmoothTime);
+
+                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+            }
+
+            Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+            _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+
+            _animator.SetFloat(_animIDSpeed, _animationBlend);
+        }
+
+        private void GravityHandler()
+        {
+            if (!_controller.isGrounded)
+            {
+                _verticalVelocity -= gravity * Time.deltaTime;
+            }
+        }
+    }
+}
